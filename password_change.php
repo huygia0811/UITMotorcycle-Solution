391e3773482285b6988078df6c73d6415f7ba1f8
AnTTViiii <20520856@gm.uit.edu.vn>
2022-12-12 03:54:59 +0700

    modify SQL_UITMOTORCYCLE.sql

--- password_change.php
+++ password_change.php
@@ -8,6 +8,7 @@ session_start();
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="./asset/DB-Picture/logo.ico" type="image/x-icon">
         <link rel="stylesheet" href="CSS/style_header.css">
         <!-- bootstrap cdn -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
--- signup.php
+++ signup.php
@@ -11,6 +11,7 @@ session_start();
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="./asset/DB-Picture/logo.ico" type="image/x-icon">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
             rel="stylesheet" />
