ddc37a6ee1cadc9fdde7b8db9c6fa2c740e613cc
anhthu1402 <20520792@gm.uit.edu.vn>
2022-12-15 15:24:23 +0700

    modify css page admin login

--- Admin/login.php
+++ Admin/login.php
@@ -3,11 +3,11 @@ ob_start();
 session_start();
 include('../includes/connect_database.php');
 $error_message='';
-if(isset($_POST['form1'])) {
+if(isset($_POST['login'])) {
     $username=$_POST['tdn'];
     $pwd=$_POST['password'];
     if(empty($_POST['tdn']) || empty($_POST['password'])) {
-        $error_message = 'Vui lòng điền đầy đủ<br>';
+        $error_message = 'Vui lòng điền đầy đủ thông tin<br>';
     } else {
 		
         $check_query="select * from `taikhoan` where tendangnhap='$username' and is_admin=1";
@@ -41,12 +41,13 @@ if(isset($_POST['form1'])) {
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Login</title>
 
-	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Login Admin</title>
+
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!--bootstrap  css link-->
         <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -59,37 +60,38 @@ if(isset($_POST['form1'])) {
             integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
             crossorigin="anonymous" referrerpolicy="no-referrer" />
         <!-- css -->
-</head>
+        <link rel="stylesheet" href="../CSS/admin_login.css">
+    </head>
 
-<body class="hold-transition login-page sidebar-mini">
+    <body class="container">
 
-<div class="login-box">
-	<div class="login-logo">
-		<b>Admin Panel</b>
-	</div>
-  	<div class="login-box-body">
-    	<p class="login-box-msg">Log in to start your session</p>
-        <?php 
+        <div class="login-box">
+            <div class="login-logo text-center m-3">
+                <h2>Admin Panel</h2>
+            </div>
+            <div class="login-box-body">
+                <p class="login-box-msg mb-3">Đăng nhập để bắt đầu phiên của bạn</p>
+
+                <form action="" method="post">
+
+                    <div class="form-group d-flex flex-row justify-content-between align-items-center mb-4 mx-auto">
+                        <input class="form-control" placeholder="Tên đăng nhập" name="tdn" type="text"
+                            autocomplete="off" autofocus>
+                    </div>
+                    <div class="form-group d-flex flex-row justify-content-between align-items-center mb-4 mx-auto">
+                        <input class="form-control" placeholder="Mật khẩu" name="password" type="password"
+                            autocomplete="off" value="">
+                    </div>
+                    <button class="btn btn-success button" type="submit" name="login">Đăng nhập</button>
+                    <button class="btn btn-sm btn-success button_sm" type="submit" name="login">Đăng nhập</button>
+                </form>
+                <?php 
 	    if( (isset($error_message)) && ($error_message!='') ):
-	        echo '<div class="error">'.$error_message.'</div>';
+	        echo '<div class="alert alert-danger text-center mt-3">'.$error_message.'</div>';
 	    endif;
 	    ?>
-		<form action="" method="post">
-			
-			<div class="form-group has-feedback">
-				<input class="form-control" placeholder="Tên đăng nhập" name="tdn" type="text" autocomplete="off" autofocus>
-			</div>
-			<div class="form-group has-feedback">
-				<input class="form-control" placeholder="Mật khẩu" name="password" type="password" autocomplete="off" value="">
-			</div>
-			<div class="row">
-				<div class="col-xs-8"></div>
-				<div class="col-xs-4">
-					<input type="submit" class="btn btn-success btn-block btn-flat login-button" name="form1" value="Log In">
-				</div>
-			</div>
-		</form>
-	</div>
-</div>
-</body>
+            </div>
+        </div>
+    </body>
+
 </html>
