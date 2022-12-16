36777c809d66c5e84faf47e226d3bb7431cfc239
huygia0811 <20520203@gm.uit.edu.vn>
2022-12-16 23:24:13 +0700

    Them phan bien dong so du

--- mypayment.php
+++ mypayment.php
@@ -6,18 +6,18 @@
             <h5>Thông tin số dư tài khoản</h5>
             <div class="position-relative">
                 <?php
-                      $get_tdn = $_SESSION['username'];
-                      $select_makh = "select * from `taikhoan` where tendangnhap='$get_tdn'";
-                      $select_makh_run = mysqli_query($con, $select_makh);
-                      $row = mysqli_fetch_assoc($select_makh_run);
-                      $makh = $row['MAKH'];
-                      $select_khachhang="select * from `khachhang` where MAKH='$makh'";
-                      $select_khachhang_run=mysqli_query($con,$select_khachhang);
-                      $row_khachhang=mysqli_fetch_assoc($select_khachhang_run);
-                      $get_sodu=$row_khachhang['SODU'];
+                $get_tdn = $_SESSION['username'];
+                $select_makh = "select * from `taikhoan` where tendangnhap='$get_tdn'";
+                $select_makh_run = mysqli_query($con, $select_makh);
+                $row = mysqli_fetch_assoc($select_makh_run);
+                $makh = $row['MAKH'];
+                $select_khachhang = "select * from `khachhang` where MAKH='$makh'";
+                $select_khachhang_run = mysqli_query($con, $select_khachhang);
+                $row_khachhang = mysqli_fetch_assoc($select_khachhang_run);
+                $get_sodu = $row_khachhang['SODU'];
                 ?>
                 <p>Số dư tài khoản hiện tại:</p>
-                <h5 class="text-danger"><?php echo currency_format($get_sodu )?></h5>
+                <h5 class="text-danger"><?php echo currency_format($get_sodu) . " đ" ?></h5>
                 <div class="position-absolute bottom-0 end-0">
                     <a class="bg-success p-2 rounded" href="">Nạp tiền</a>
                 </div>
@@ -32,8 +32,6 @@
                             <thead class="thead-dark">
                                 <tr role="row" style='text-align:center'>
                                     <th width="50" scope="col">
-                                        Mã khách hàng</th>
-                                    <th width="50" scope="col">
                                         Thời gian</th>
                                     <th width="50" scope="col">
                                         Mô tả</th>
@@ -41,6 +39,32 @@
                                         Số dư</th>
                                 </tr>
                             </thead>
+                            <tbody>
+                                <?php
+                                $select_hoadon = "SELECT * FROM HOADON WHERE MAKH = '$makh'";
+                                $select_hoadon_run = mysqli_query($con, $select_hoadon);
+                                while ($row_hoadon = mysqli_fetch_assoc($select_hoadon_run)) {
+                                ?>
+                                    <tr class="text-center">
+                                        <td><?php echo $row_hoadon['NGHD'] ?></td>
+                                        <td>Mua hàng</td>
+                                        <td><?php echo currency_format($row_hoadon['TRIGIA']) . " đ" ?></td>
+                                    </tr>
+                                <?php
+                                }
+                                $select_naptien = "SELECT * FROM NAPTIEN WHERE MAKH = '$makh' AND DADUYET = 1";
+                                $select_naptien_run = mysqli_query($con, $select_naptien);
+                                while ($row_naptien = mysqli_fetch_assoc($select_naptien_run)) {
+                                ?>
+                                    <tr class="text-center">
+                                        <td><?php echo $row_naptien['NGAYNAP'] ?></td>
+                                        <td>Nạp tiền thành công</td>
+                                        <td><?php echo currency_format($row_naptien['SOTIEN']) . " đ" ?></td>
+                                    </tr>
+                                <?php
+                                }
+                                ?>
+                            </tbody>
                         </table>
                     </div>
                 </div>
@@ -58,8 +82,6 @@
                             <tr role="row" style='text-align:center'>
                                 <th width="50" scope="col">
                                     Mã nạp tiền</th>
-                                <th width="50" scope="col">
-                                    Mã khách hàng</th>
                                 <th width="250" scope="col">
                                     Ngày nạp</th>
                                 <th width="160" scope="col">
@@ -72,11 +94,6 @@
                         </thead>
                         <tbody>
                             <?php
-                            $get_tdn = $_SESSION['username'];
-                            $select_makh = "select * from `taikhoan` where tendangnhap='$get_tdn'";
-                            $select_makh_run = mysqli_query($con, $select_makh);
-                            $row = mysqli_fetch_assoc($select_makh_run);
-                            $makh = $row['MAKH'];
                             $select_naptien = "select * from `naptien` where MAKH='$makh'";
                             $select_naptien_run = mysqli_query($con, $select_naptien);
                             while ($row_naptien = mysqli_fetch_assoc($select_naptien_run)) {
@@ -84,10 +101,9 @@
 
                                 <tr class="text-center">
                                     <td><?php echo $row_naptien['MANAPTIEN'] ?></td>
-                                    <td><?php echo $row_naptien['MAKH'] ?></td>
                                     <td><?php echo $row_naptien['NGAYNAP'] ?></td>
                                     <td><?php echo $row_naptien['SOTAIKHOAN'] ?></td>
-                                    <td><?php echo $row_naptien['SOTIEN'] ?></td>
+                                    <td><?php echo currency_format($row_naptien['SOTIEN']) . " đ"?></td>
                                     <td><?php if ($row_naptien['DADUYET'] == 1) {
                                             echo "Thành công";
                                         } else {
